6b3af8cb81690f0c44481b582357e05bee35dbe0
unknown <thalesmauriciojodar@gmail.com>
2025-09-16 22:33:26 -0300

    Organização da pasta...

---
+++ MANUAL DE AULAS/09-16-.py
@@ -0,0 +1,65 @@
+# ALGORITMOS DE BUSCA 
+    # BUSCA LINEAR/PESQUISA SEQUENCIAL(funciona ordenado e não ordenado):
+vetor = [15, 10, 3, 12, 5, 7, 1]
+valor = int(input("Informe um número: "))
+
+#VERSÃO 1: Vetor não ordenado
+encontrei = False 
+posicao = None
+for i in range(0, len(vetor)):
+    if(valor == vetor[i]):
+        encontrei = True
+        posicao = i
+        break
+if(encontrei):
+    print(f"Encontrado no vetor no indice {posicao}°.")
+
+else:
+    print("Não encontrado no vetor.")
+
+#VERSÃO 2: Vetor ordenado
+vet = [1, 3 ,5 ,7, 10, 12, 15]
+encontrei = False 
+posicao = None
+for i in range(0, len(vet)):
+    if(valor == vet[i]):
+        encontrei = True
+        posicao = i
+        break
+    elif(valor < vet[i]):
+        break
+if(encontrei):
+    print(f"Encontrado no vetor no indice {posicao}°.")
+
+else:
+    print("Não encontrado no vetor.")
+
+
+    #BUSCA BINÁRIA(funciona só com vetor ordenado)
+        #3 váriaveis auxiliares inicio(regiao de busca), meio(regiao de busca), fim(regiao de busca) SÃO INDICIES E NÃO OS VALORES
+
+        # inicio = 0 (caso seja mais que o meio) depois se torna i = m+1
+        # meio = int((inicio + fim)/2)
+        # fim = len(vetor)-1 (caso seja menor que o meio) depois se torna f= m-1
+print("Busca binária")
+encontrei = False
+posicao = -1 #-None
+inicio = 0
+fim = len(vet) - 1
+while(inicio <= fim):
+    meio = int((inicio + fim) / 2)
+
+    if(vet[meio] == valor):
+        encontrei = True
+        posicao = meio
+        break
+    elif(vet[meio] > valor):
+        fim = meio - 1
+    else:
+        inicio = meio + 1
+        
+if(encontrei):
+    print(f"Encontrado no vetor no indice {posicao}°.")
+
+else:
+    print("Não encontrado no vetor.")
---
+++ MANUAL DE AULAS/09-16.py
@@ -0,0 +1,58 @@
+#16/09 -- MÉTODOS UTILIZADOS PARA ORDENAR VETOR
+#BOLHA
+#VALORES PRIMITIVOS: int, float, str, bool
+def bolha(vetor):
+    for ciclo in range(0, len(vetor)-1 ):
+        for i in range(0, len(vetor)-1):
+            if(vetor[i] > vetor[i+1]):  #DECRESCENTE = (vetor[i] < vetor[i+1])
+                copia = vetor[i]
+                vetor[i] = vetor[i+1]
+                vetor[i+1] = copia
+
+vet = [10, 9, 8, 7, 6, 7, 8]
+print(vet) #Envia o valor original de referência
+bolha(vet) #A função muda o original por referência
+print(vet) #Envia o valor modificado pela função
+
+
+
+#INSERÇÃO
+
+def insercao(vetor):
+    for i in range(1, len(vetor)):
+        temp = vetor[i] #armazena o valor
+        j = i - 1 #guarda o i anterior
+        while(j >= 0 and temp < vetor[j]):  #tem que parar antes de -1 ou se tiver um menor
+            vetor[j+1] = vetor[j] #j representa possição anterior então, guarda na posição da frente
+            j = j - 1 #e conta um pra trás
+        vetor[j+1] = temp #se não funcionar mantém ele na mesma posição
+    
+        
+vetor = [15, 10, 3, 12, 5, 7, 1]
+print(vetor)
+insercao(vetor)
+print(vetor)
+
+
+#SELEÇÃO
+#2 variaveis controle MENOR e INDICIE DO MENOR VALOR
+def selecao(vetor):
+    for i in range(0, len(vetor)-1):
+        menor = vetor[i]
+        imenor = i
+
+        for j in range(i + 1, len(vetor)): #percorre onde está +1 para encontrar menor valor
+            if(vetor[j] < menor):
+                vetor[j] = menor
+                imenor = j
+
+    vetor[imenor] = vetor[i] #valor com o menor indicie recebe
+    vetor[i] = menor # valor com os primeiros indicies recebe o menor
+
+print(vetor)
+selecao(vetor)
+print(vetor)
+
+
+
+
